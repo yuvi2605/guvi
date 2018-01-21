d9881767318e0dc1f0d9005c98078d7dcafe442d
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-01-21 16:38:34 +0530

    Create evo.c

---
+++ evo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int x;
+clrscr();
+printf("\n enter the value of x");
+scanf("%d",&x);
+if(x%2==0)
+{
+printf("\n the value is even");
+}
+else
+{
+printf("\n the value is odd");
+}
+getch();
+}
