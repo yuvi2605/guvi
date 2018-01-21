80e9aabec489f82a7e21f69145a625fef3102008
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-01-21 16:41:57 +0530

    Update evo.c

--- evo.c
+++ evo.c
@@ -2,16 +2,14 @@
 void main()
 {
 int x;
-clrscr();
-printf("\n enter the value of x");
 scanf("%d",&x);
 if(x%2==0)
 {
-printf("\n the value is even");
+printf("even");
 }
 else
 {
-printf("\n the value is odd");
+printf("odd");
 }
 getch();
 }
